20ff75693a05e375843f26e8d97d6b78fcde9baf
ThiagoIanuch <thiagoianuch@hotmail.com>
2025-09-30 18:42:53 -0300

    Criar a API

---
+++ frontend/src/app/services/api.ts
@@ -0,0 +1,13 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class Api {
+  constructor(private httpClient: HttpClient) {}
+
+  getProducts() {
+    return this.httpClient.get('assets/products.json');
+  }
+}
