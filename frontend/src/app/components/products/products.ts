c47a2cee43993e014b7a211130f3f94a0fc0de02
ThiagoIanuch <thiagoianuch@hotmail.com>
2025-09-30 18:45:30 -0300

    Integrar a API e listar os produtos

--- frontend/src/app/components/products/products.ts
+++ frontend/src/app/components/products/products.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { Api } from '../../services/api';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-products',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './products.html',
   styleUrl: './products.css'
 })
 export class Products {
-  
+  constructor(private api: Api) {}
+
+  products: any[] = [];
+
+  ngOnInit() {
+    this.api.getProducts().subscribe(data => {
+      this.products = data as any[];
+    })
+  }
 }
