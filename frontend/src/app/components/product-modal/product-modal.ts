79a3d235755202e4000dc4b05b86f6bde8b10115
ThiagoIanuch <thiagoianuch@hotmail.com>
2025-10-01 21:50:33 -0300

    Alterar formulário para reativo

--- frontend/src/app/components/product-modal/product-modal.ts
+++ frontend/src/app/components/product-modal/product-modal.ts
@@ -1,18 +1,34 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { Product } from '../../interfaces/product';
 
 @Component({
   selector: 'app-product-modal',
-  imports: [FormsModule],
+  imports: [ReactiveFormsModule],
   templateUrl: './product-modal.html',
   styleUrl: './product-modal.css'
 })
 export class ProductModal {
-  @Input() product: Product = { name: '', description: '', quantity: 0, price: 0 };
+  @Input() product: Product | null = null;
   @Output() save = new EventEmitter<Product>();
 
+  productForm: FormGroup = new FormGroup({
+    id: new FormControl(null),
+    name: new FormControl(''),
+    description: new FormControl(''),
+    quantity: new FormControl(''),
+    price: new FormControl('')
+  });
+
+  ngOnInit() {
+    if (this.product) {
+      this.productForm.patchValue(this.product);
+    }
+  }
+
   onSubmit() {
-    this.save.emit(this.product);
+    if (this.productForm.valid) {
+      this.save.emit(this.productForm.value as Product);
+    }
   }
 }
